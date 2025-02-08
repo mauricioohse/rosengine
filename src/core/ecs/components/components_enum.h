b9317607ef36b01ea2d7c877fe42ff8219049c80
mauricioohse <mauricioohsexd@hotmail.com>
2025-02-08 16:33:32 -0300

    delete old files

--- src/core/ecs/components/components_enum.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define xcomponent(name, type, id, ...) name,
-enum component_type
-{
-#include "components.def"
-    COMPONENT_LAST_VALUE // always is the last value on the enum
-};
-#undef xcomponent
